5abda0e6fe073aae5396bbd9e163a27400322045
FarukSavur <43204188+FarukSavur@users.noreply.github.com>
2023-11-23 17:31:51 +0300

    link componenti konusu işlendi.

--- resources/js/Pages/Layout/Navbar.vue
+++ resources/js/Pages/Layout/Navbar.vue
@@ -1,4 +1,19 @@
 <template>
-
-    navbar
+  <ul>
+    <li>
+      <Link href="/">Home</Link>
+    </li>
+    <li>
+     <Link href="/user">User page</Link>
+    </li>
+  </ul>
 </template>
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+  components: {
+    Link,
+  },
+};
+</script>
